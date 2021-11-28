f9c4ded1c9a9bb9e0c36df92b5857989cdba2db3
Saeejith Nair <saeejithn@gmail.com>
2021-11-28 03:29:29 -0800

    Updated job scripts

--- submit_job.sh
+++ submit_job.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-#SBATCH --account=def-someuser
-#SBATCH --mem-per-cpu=1.5G      # increase as needed
-#SBATCH --time=1:00:00
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:p100:1 # request a GPU
+
+#SBATCH --account=vqw-634
+#SBATCH --cpus-per-task=24 # increase this parameter and increase "--num_workers" accordingly to see the effect on performance
+
+#SBATCH --mem=16G      
+#SBATCH --time=24:0:0
+
 
 module load python/3.9.6
 
@@ -9,8 +15,17 @@ virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 
-pip install --no-index -r MLRC2021-SimpleCopyPaste/requirements.txt
+cd /home/snair/projects/def-mjshafie/snair/syde671
 
 cp cityscapes.tar.gz $SLURM_TMPDIR/
 tar -xzf $SLURM_TMPDIR/cityscapes.tar.gz
+
+cp -rf MLRC2021-SimpleCopyPaste/ $SLURM_TMPDIR/
+cd $SLURM_TMPDIR/MLRC2021-SimpleCopyPaste/
+
+pip install --no-index -r requirements.txt
+
+python trainval.py snair-test -rt $SLURM_TMPDIR/cityscapes --num_workers 22
+
+
 
