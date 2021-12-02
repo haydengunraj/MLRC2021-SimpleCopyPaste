2cd22d84267237c0cd6a34a18e8f4fffab428782
Saeejith Nair <saeejithn@gmail.com>
2021-12-02 02:29:41 -0500

    Added mobilenet fractional configs

--- trainval.py
+++ trainval.py
@@ -15,6 +15,7 @@ import detection.utils as utils
 TORCH_HOME = 'weights'
 LOSS_KEYS = ('loss_classifier', 'loss_box_reg', 'loss_mask', 'loss_objectness', 'loss_rpn_box_reg')
 EXPERIMENT_DIR = 'experiments'
+CONFIG_DIR = 'configurations'
 
 
 def save_checkpoint(output_dir, epoch, step, model, optimizer, lr_scheduler, scaler=None, enable_wandb=False):
@@ -54,7 +55,8 @@ def trainval_cityscapes(args):
 
     # Load configuration
     experiment_dir = os.path.join(EXPERIMENT_DIR, experiment_name)
-    config_file = os.path.join(experiment_dir, 'config.yaml')
+    config_dir = os.path.join(CONFIG_DIR, experiment_name)
+    config_file = os.path.join(config_dir, 'config.yaml')
     if not os.path.exists(config_file):
         raise ValueError('Config file not found: {}'.format(config_file))
     with open(config_file, 'r') as f:
